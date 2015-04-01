789878bbfcf32f012c3cfcec777cc4738d6a5d87
Teddy <tedsterDP@gmail.com>
2015-04-01 10:59:24 -0700

    Initial Commit

---
+++ CS261-1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CS261-1
+//
+//  Created by Teddy on 4/1/15.
+//  Copyright (c) 2015 Teddy Duchow-Pressley. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
