db36560dc594033f1a24e069cc3815f00980071e
Keeley Abbott <abbottk@eecs.oregonstate.edu>
2015-04-08 17:02:56 -0700

    correct_weird_commit

--- assignment_1/Q4.c
+++ assignment_1/Q4.c
@@ -1,34 +1,70 @@
-/* CS261- Assignment 1 - Q.4*/
-/* Name:
- * Date:
- * Solution description:
- */
- 
+/* CS261- Assignment 1 - Q.4 */
+/* Name: Keeley Abbott and Theodore Duchow-Pressley
+   Date: 2014 April 6
+   Solution description: Create an array of struct students, assign them all
+                         randomly generated IDs and scores, then sort them from
+                         smallest to largest scores. Finally print out both the
+                         IDs and scores before and after sorting.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 struct student{
-	int id;
-	int score;
+  int id;
+  int score;
 };
 
 void sort(struct student* students, int n){
-     /*Sort the n students based on their score*/     
-     /* Remember, each student must be matched with their original score after sorting */
+  /* Sort the n students based on their score */
+  /* Remember, each student must be matched with their original score
+     after sorting
+  */
+  struct student curr;
+
+  for(int i = 0; i < n; i++){
+    students[i] = curr;
+    for(int j = (i + 1); j < n; j++){
+      if(students[j].score < curr.score){
+        students[i] = students[j];
+        students[j] = curr;
+        curr = students[i];
+      }
+    }
+  }
 }
 
 int main(){
-    /*Declare an integer n and assign it a value.*/
-    
-    /*Allocate memory for n students using malloc.*/
-    
-    /*Generate random IDs and scores for the n students, using rand().*/
-    
-    /*Print the contents of the array of n students.*/
+  srand(time(NULL));
 
-    /*Pass this array along with n to the sort() function*/
-    
-    /*Print the contents of the array of n students.*/
-    
-    return 0;
+  /* Declare an integer n and assign it a value. */
+  int n = 20;
+
+  /* Allocate memory for n students using malloc. */
+  struct student *Students = malloc(sizeof(struct student)*n);
+
+  /* Generate random IDs and scores for the n students, using rand(). */
+  for(int i = 0; i < n; i++){
+      Students[i].id = i + 1;
+      Students[i].score = rand()%100;
+    }
+
+  /* Print the contents of the array of n students. */
+  printf("Unsorted:\n");
+  for(int i = 0; i < n; i++){
+      printf("ID%d Score%d\n", Students[i].id, Students[i].score);
+    }
+  printf("\n");
+
+  /* Pass this array along with n to the sort() function */
+  sort(Students, n);
+
+  /* Print the contents of the array of n students. */
+  printf("Sorted:\n");
+  for(int i = 0; i < n; i++){
+      printf("ID%d Score%d\n", Students[i].id, Students[i].score);
+    }
+
+  return 0;
 }
