c1d2237679be487d10e9f8da1a27e4a60b665b52
Teddy <tedsterDP@gmail.com>
2015-04-08 17:13:06 -0700

    Q5 functional and documented

--- assignment_1/Q5.c
+++ assignment_1/Q5.c
@@ -1,22 +1,14 @@
 /* CS261- Assignment 1 - Q.5*/
-/* Name: 
- * Date:
- * Solution description:
+/* Name: Keeley Abbott and Theodore Duchow-Pressley
+ * Date: 2015 April, 8
+ * Solution description: reads a word from the keyboard using scanf
+ 						 converts it to StUdLy CaPs 
  */
  
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-/*converts ch to upper case, assuming it is in lower case currently*/
-char toUpperCase(char ch){
-     return ch-'a'+'A';
-}
-
-/*converts ch to lower case, assuming it is in upper case currently*/
-char toLowerCase(char ch){
-     return ch-'A'+'a';
-}
 
 void studly(char* word){
      /*Convert to studly caps*/
